5cdb3f2fc0c989a3cf09fa7611b95cb38d99638a
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-06-28 16:54:31 +0200

    home completata

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,60 @@
     <h1 class="text-center p-1" style="color:red">{{ $titolo }}</h1>
     <hr style="color: white;">
 
+   <div class="card_container">
+        <div class="card">
+            <div class="img_container">
+                <img src="/img/oshi_no_ko.jpg" alt="">
+            </div>
+            @foreach($fumetto1 as $fumetto)
+            <li>{{$fumetto}}</li>
+            @endforeach
+        </div>
+        <div class="card">
+        <div class="img_container">
+                <img src="/img/berserk.jpg" alt="">
+            </div>
+            @foreach($fumetto2 as $fumetto)
+            <li>{{$fumetto}}</li>
+            @endforeach
+        </div>
+        <div class="card">
+        <div class="img_container">
+                <img src="/img/the_climber.jpg" alt="">
+            </div>
+            @foreach($fumetto3 as $fumetto)
+            <li>{{$fumetto}}</li>
+            @endforeach
+        </div>
+        <div class="card">
+        <div class="img_container">
+                <img src="/img/20th_century_boys.jpg" alt="">
+            </div>
+            @foreach($fumetto4 as $fumetto)
+            <li>{{$fumetto}}</li>
+            @endforeach
+        </div>
+        <div class="card">
+        <div class="img_container">
+                <img src="/img/monster.jpg" alt="">
+            </div>
+            @foreach($fumetto5 as $fumetto)
+            <li>{{$fumetto}}</li>
+            @endforeach
+        </div>
+        <div class="card">
+        <div class="img_container">
+                <img src="/img/hirayasumi.jpg" alt="">
+            </div>
+            @foreach($fumetto6 as $fumetto)
+            <li>{{$fumetto}}</li>
+            @endforeach
+        </div>
 
+
+
+     
+   </div>
 
     <a href="/about"> => pg 2</a>
 
--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,50 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/home', function () {
-  $data = [
-    "titolo"=>"WELCOME LARAVEL COMICS"
-  ];
+    $data = [
+        "titolo" => "WELCOME LARAVEL COMICS",
+        "fumetto1" => [
+            "Titolo: Oshi No Ko",
+            "Autore: Aka Akasaka",
+            "Prezzo: €9.90",
+        ],
+        "fumetto2" => 
+        [
+            "Titolo: Berserk",
+            "Autore: Kentaro Miura",
+            "Prezzo: €5.50",
+         
+        ],
+        "fumetto3" => [
+            "Titolo: The Climber",
+            "Autore: Shin'ickìhi Sakamoto",
+            "Prezzo: €6.90",
+           
+
+        ],
+        "fumetto4" => [
+            "Titolo: 20th Century Boys Ultimate Deluxe",
+            "Autore: Naoki Urasawa",
+            "Prezzo: €14.90",
+        ],
+        "fumetto5" => [
+            "Titolo: Monster Deluxe",
+            "Autore: Naoki Urasawa",
+            "Prezzo: €13.90",
+        ],
+        "fumetto6" => [
+            "Titolo: Hirayasumi",
+            "Autore: Keigo Shinzo",
+            "Prezzo: €6.90",
+        ],
+        
+    ];
     return view('home', $data);
 });
 
 Route::get('/about', function () {
     $data = [
-        "titolo"=>"WELCOME LARAVEL COMICS pg2"
-      ];
-        return view('altro', $data);
-  });
-
+        "titolo" => "WELCOME LARAVEL COMICS pg2"
+    ];
+    return view('altro', $data);
+});
